a5fe27d70cbb5b2f9178ba4854d321299369a45c
Robin Spainhour <robin@robinspainhour.com>
2009-10-25 00:24:33 +0200

    Added image file for rspec use. New route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'static'
+  
+  map.resources :banner_ads
 
   # The priority is based upon order of creation: first created -> highest priority.
 
